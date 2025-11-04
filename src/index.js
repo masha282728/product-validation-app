f59242bf932783f10e1dc19de47dac20d9dddeef
masha282728 <masamartysko@gmail.com>
2025-11-04 10:52:36 +0100

    Fix: use Render URL for frontend API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 const host = '0.0.0.0';
 app.listen(port, host, () => {
   console.log(`✅ Server listening on port ${port}`);
